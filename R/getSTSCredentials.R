1043cd7dfb4d9c519b28ebd5939c84a9dfb5df46
Alonzo Barnett <alonzo.r.barnett@gmail.com>
2018-05-17 20:33:51 -0400

    helper to get STS credentials

---
+++ R/getSTSCredentials.R
@@ -0,0 +1,27 @@
+#'The structure of an STS request and response is fully described at:
+#'http://docs.aws.amazon.com/STS/latest/APIReference/API_AssumeRole.html
+#'
+getSTSCredentials <- function(rootCredentials = NULL, roleArn = NULL, RoleSessionName = NULL, MFADeviceSerialNumber = NULL, Duration = "3600") {
+    
+    stsContent <- stsGenContentString(
+        roleArn = roleArn, RoleSessionName = RoleSessionName,
+        MFADeviceSerialNumber = MFADeviceSerialNumber,
+        Duration = Duration
+    )
+
+    startTime <- as.POSIXlt(Sys.time(), tz = "GMT")
+    tmpcreds <- awsRestRequest(
+        credentials = rootCredentials, httpMethod = "POST", service = "sts",
+        optionalHeaders = c("content-type" = "application/x-www-form-urlencoded; charset=utf-8"),
+        content = stsContent, conversionFUN = XML::xmlToList
+    )
+    
+    tmpcreds <- tmpcreds$AssumeRoleResult$Credentials
+    names(tmpcreds)[grep("^access", names(tmpcreds), ignore.case = T)] <- "AWS_ACCESS_KEY_ID"
+    names(tmpcreds)[grep("^secret", names(tmpcreds), ignore.case = T)] <- "AWS_SECRET_ACCESS_KEY"
+    names(tmpcreds)[grep("token$", names(tmpcreds), ignore.case = T)] <- "Token"
+    
+    tmpcreds$Expiration <- as.POSIXlt(tmpcreds$Expiration, tz = "GMT", format = "%Y-%m-%dT%H:%M:%S")
+    
+    return(c(list(LastUpdated = startTime), tmpcreds))
+}
