dd3adb8da588b2b597ecc931c7f0288373368d95
Alonzo Barnett <alonzo.r.barnett@gmail.com>
2018-07-29 14:03:52 -0400

    add package level doc

---
+++ R/RcurlAWS.R
@@ -0,0 +1,14 @@
+#' RcurlAWS: a package for R intended to facilitate use of AWS services without external dependencies.
+#'
+#' Primary development is around credential management (both root and STS), somewhat robust use of cerdential/config files, and generic API requests.  Aws Root and Temporary credential management implemented using R6 classes.  STS is supported to generate temporary credentials.
+#'
+#'Helper functions to access AWS services via package::curl  
+#'  * limited functionality is implemented in wrappers which obscure the worker functions  
+#'  * In theroy any HTTP(S) style request to the AWS REST API is possible with awsRestRequest  
+#'  * NEED TO add service-level [S3, SQS, etc.] modules  
+#'  * NEED TO ensuring all settings in a config file are used in service calls  
+#'
+#' @docType package
+#' @name RcurlAWS
+#'
+NULL
