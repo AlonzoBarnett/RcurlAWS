adae50499a773779495d67c5bb965d5e6a3a3132
Alonzo Barnett <alonzo.r.barnett@gmail.com>
2018-08-04 13:27:27 -0400

    format list

--- R/RcurlAWS.R
+++ R/RcurlAWS.R
@@ -3,10 +3,13 @@
 #' Primary development is around credential management (both root and STS), somewhat robust use of cerdential/config files, and generic API requests.  Aws Root and Temporary credential management implemented using R6 classes.  STS is supported to generate temporary credentials.
 #'
 #'Helper functions to access AWS services via package::curl  
-#'  * limited functionality is implemented in wrappers which obscure the worker functions  
-#'  * In theroy any HTTP(S) style request to the AWS REST API is possible with awsRestRequest  
-#'  * NEED TO add service-level [S3, SQS, etc.] modules  
-#'  * NEED TO ensuring all settings in a config file are used in service calls  
+#'
+#' \itemize{
+#'   \item limited functionality is implemented in wrappers which obscure the worker functions 
+#'   \itemIn theroy any HTTP(S) style request to the AWS REST API is possible with awsRestRequest 
+#'   \item NEED TO add service-level [S3, SQS, etc.] modules 
+#'   \item NEED TO ensuring all settings in a config file are used in service calls 
+#' }
 #'
 #' @docType package
 #' @name RcurlAWS
